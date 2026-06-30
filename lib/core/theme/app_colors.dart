283a68c9e342eb9b1b90007431019b3394c26f61
caledcresst-dev <caled.cresst@gmail.com>
2026-06-30 11:37:58 -0300

    Modo escuro e adição de pacotes

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Cores de Fundo (Tema Escuro)
+  static const Color background = Color(0xFF121212); 
+  static const Color surface = Color(0xFF1E1E1E);   
+  
+  // Cores de Destaque
+  static const Color primary = Color(0xFF00E676);   
+  static const Color primarySecondary = Color(0xFF1B5E20); 
+  
+  // Textos
+  static const Color textPrimary = Color(0xFFFFFFFF);  
+  static const Color textSecondary = Color(0xFFB3B3B3); 
+  
+  // Bordas e Divisores
+  static const Color border = Color(0xFF2C2C2C);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meu_treino/core/navigation/main_screen.dart';
 import 'features/workout/presentation/workout_screen.dart';
+import 'package:meu_treino/core/theme/app_colors.dart';
 
 void main(){
   runApp(const MyWorkout());
@@ -15,9 +16,16 @@ class MyWorkout extends StatelessWidget{
       title: 'IronProgress',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.white,
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: AppColors.background,
+      cardColor: AppColors.surface,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        iconTheme: IconThemeData(color: AppColors.textPrimary),
+        titleTextStyle: TextStyle(color: AppColors.textPrimary, fontSize: 20, fontWeight: FontWeight.bold),
       ),
+    ),
       home: const MainScreen(),
     );
   }
